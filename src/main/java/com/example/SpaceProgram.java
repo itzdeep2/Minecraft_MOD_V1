8530feae3b016ce9b34e5de8174a64365a03bbbc
Dhritideep Pathak <madhupathak316@gmail.com>
2026-09-23 21:44:59 +0530

    feat(registry): register rocket entity and launch pad using BuiltInRegistries

--- src/main/java/com/example/SpaceProgram.java
+++ src/main/java/com/example/SpaceProgram.java
@@ -1,38 +1,41 @@
 package com.example;
 
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
-import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricEntityTypeBuilder;
-import net.minecraft.block.Block;
-import net.minecraft.entity.EntityDimensions;
-import net.minecraft.entity.EntityType;
-import net.minecraft.entity.SpawnGroup;
-import net.minecraft.item.BlockItem;
-import net.minecraft.registry.Registries;
-import net.minecraft.registry.Registry;
-import net.minecraft.util.Identifier;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.EntityDimensions;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.MobCategory;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockBehaviour;
 
 public class SpaceProgram implements ModInitializer {
     public static final String MOD_ID = "spaceprogram";
-    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-    public static final Block LAUNCH_PAD = new LaunchPadBlock(FabricBlockSettings.create().strength(4.0f).requiresTool());
+    public static final Block LAUNCH_PAD = new LaunchPadBlock(BlockBehaviour.Properties.of().strength(4.0f).requiresCorrectToolForDrops());
 
     public static final EntityType<RocketEntity> ROCKET = Registry.register(
-            Registries.ENTITY_TYPE,
-            new Identifier(MOD_ID, "rocket"),
-            FabricEntityTypeBuilder.create(SpawnGroup.MISC, RocketEntity::new)
+            BuiltInRegistries.ENTITY_TYPE,
+            new ResourceLocation(MOD_ID, "rocket"),
+            FabricEntityTypeBuilder.create(MobCategory.MISC, RocketEntity::new)
                     .dimensions(EntityDimensions.fixed(1.0f, 3.0f))
                     .build()
     );
 
     @Override
     public void onInitialize() {
-        Registry.register(Registries.BLOCK, new Identifier(MOD_ID, "launch_pad"), LAUNCH_PAD);
-        Registry.register(Registries.ITEM, new Identifier(MOD_ID, "launch_pad"), new BlockItem(LAUNCH_PAD, new FabricItemSettings()));
-        LOGGER.info("Space Program Initialized. Flight systems online.");
+        Registry.register(BuiltInRegistries.BLOCK, new ResourceLocation(MOD_ID, "launch_pad"), LAUNCH_PAD);
+        BlockItem padItem = new BlockItem(LAUNCH_PAD, new Item.Properties());
+        Registry.register(BuiltInRegistries.ITEM, new ResourceLocation(MOD_ID, "launch_pad"), padItem);
+
+        ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.FUNCTIONAL_BLOCKS).register(content -> {
+            content.accept(LAUNCH_PAD);
+        });
     }
 }
